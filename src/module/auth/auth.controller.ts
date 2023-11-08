262a8338936d4b45c2f48efb766ae172ca347623
NAIMKHAN3 <naimkhan01856@gmail.com>
2023-11-09 03:05:23 +0600

    [Done] Login success

--- src/module/auth/auth.controller.ts
+++ src/module/auth/auth.controller.ts
@@ -8,8 +8,8 @@ export const createRegister = async (req: Request, res: Response, next: NextFunc
         const hashedPassword = await bcrypt.hash(req.body.password, 10)
         req.body.password = hashedPassword;
         const result = await User.create(req.body);
-        // const user = await User.findById(result._id).select('-password -__v')
         const user = {
+            _id: result._id,
             name: result.name,
             email: result.email,
             address: result.address,
@@ -25,6 +25,50 @@ export const createRegister = async (req: Request, res: Response, next: NextFunc
         res.status(201).send({
             status: true,
             message: "Register Success Done",
+            data: user,
+            accessToken
+        })
+    }
+    catch (err) {
+        next(err)
+    }
+}
+
+export const loginUser = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { email, password } = req.body;
+
+        const isUserExist = await User.findOne({ email })
+        if (!isUserExist) {
+            throw new Error('User not found')
+        }
+        const isPasswordCorrect = await bcrypt.compare(password, isUserExist.password);
+        if (!isPasswordCorrect) {
+            throw new Error("Password is incorrect")
+        }
+
+        const user = {
+            _id: isUserExist._id,
+            name: isUserExist.name,
+            email: isUserExist.email,
+            address: isUserExist.address,
+            phoneNumber: isUserExist.phoneNumber,
+        }
+
+
+        const accessToken = createToken(user, "ACCESS");
+
+        const refreshToken = createToken(user, "REFRESH");
+
+        res.cookie("refreshToken", refreshToken, {
+            httpOnly: true,
+            path: "/",
+        });
+
+        res.status(200).send({
+            success: true,
+            statusCode: 200,
+            message: "User Login Success",
             data: user,
             accessToken
         })
