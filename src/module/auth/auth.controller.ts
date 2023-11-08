1657f4af87fe50b2136dd2647bd88f07b82d9c65
NAIMKHAN3 <naimkhan01856@gmail.com>
2023-11-09 02:59:36 +0600

    Json web token use

--- src/module/auth/auth.controller.ts
+++ src/module/auth/auth.controller.ts
@@ -1,17 +1,32 @@
 import { Request, Response, NextFunction } from "express";
 import bcrypt from 'bcrypt'
 import { User } from "./auth.model";
+import { createToken } from "../../utils/token.utils";
 
 export const createRegister = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const hashedPassword = await bcrypt.hash(req.body.password, 10)
         req.body.password = hashedPassword;
-        console.log(req.body)
         const result = await User.create(req.body);
+        // const user = await User.findById(result._id).select('-password -__v')
+        const user = {
+            name: result.name,
+            email: result.email,
+            address: result.address,
+            phoneNumber: result.phoneNumber,
+        }
+        const accessToken = createToken(user, "ACCESS");
+        const refreshToken = createToken(user, "REFRESH");
+
+        res.cookie("refreshToken", refreshToken, {
+            httpOnly: true,
+            path: "/",
+        });
         res.status(201).send({
             status: true,
             message: "Register Success Done",
-            data: result
+            data: user,
+            accessToken
         })
     }
     catch (err) {
--- src/module/auth/auth.model.ts
+++ src/module/auth/auth.model.ts
@@ -8,6 +8,7 @@ const userSchema = new Schema<IUser>({
     },
     email: {
         type: String,
+        unique: true,
         required: true
     },
     password: {
