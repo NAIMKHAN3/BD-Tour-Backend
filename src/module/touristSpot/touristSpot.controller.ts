d161b26df41eea793c3904a8b378032fdaa50a92
NAIMKHAN3 <naimkhan01856@gmail.com>
2023-11-09 06:58:58 +0600

    verifyJWT Added

--- src/module/touristSpot/touristSpot.controller.ts
+++ src/module/touristSpot/touristSpot.controller.ts
@@ -17,7 +17,7 @@ export const createTouristSpot = async (req: Request, res: Response, next: NextF
 export const getTouristSpot = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const destinationId = req.params.id;
-        const result = await TouristSpot.find({ destinationId });
+        const result = await TouristSpot.find({ destinationId }).populate('destinationId');
         res.status(200).send({
             status: true,
             message: "Tourist Spot Get Success",
